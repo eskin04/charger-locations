d2730ce7d92132b9d7e8318d23b24b0bde4f985c
okan eskin <71434594+eskin04@users.noreply.github.com>
2023-12-12 04:13:03 +0300

    bug fix

--- public/maps/map.js
+++ public/maps/map.js
@@ -1,3 +1,4 @@
+
 fetch('http://localhost:3000/api/stations')
 .then(response => response.json())
 .then(data => {
@@ -23,20 +24,20 @@ fetch('http://localhost:3000/api/stations')
       model_desc: data[0][i].model_aciklama,
       plug_type: data[0][i].soket_tipi,
       plug_number: data[0][i].soket_sayisi,
-      aktiflik: data[0][i].aktiflik = 1? "Aktif" : "Kullanım Dışı",
+      aktiflik: data[0][i].aktiflik == 1? "Aktif" : "Kullanım Dışı",
       address: data[0][i].adres,
       indoor: data[0][i].lokasyon_tipi=0? "İç Mekan" : "Dış Mekan",
       hizmetsaati: hizmet_saati,
       otopark: data[0][i].otopark,
       parksure: data[0][i].park_suresi,
       
-      type: "charging-station",
+      type: data[0][i].aktiflik == 1 ? "charging-station" : "store-slash",
       position: {
         lat: lat,
         lng: lng,
       },
     })
-    id+=3
+    id++
     
 
   }
@@ -69,19 +70,31 @@ async function initMap() {
         toggleHighlight(AdvancedMarkerElement, property);
 
       });
+      
+      
+      
     }
   }
   
   var lastHighlightedMarker = null;
+  var removeHighlight = null;
 
   function toggleHighlight(markerView, property) {
-    
+    last_table = markerView.content.id;
+    console.log('tıklandı')
     if (lastHighlightedMarker) {
         lastHighlightedMarker.content.classList.remove("highlight");
         lastHighlightedMarker.zIndex = null;
       }
 
       // Tıklanan marker'ı vurgula
+      
+      if(removeHighlight && removeHighlight==markerView){
+        markerView.content.classList.remove("highlight");
+        markerView.zIndex = null;
+        removeHighlight = null;
+        return;
+      }
       markerView.content.classList.add("highlight");
       markerView.zIndex = 1;
 
@@ -93,54 +106,66 @@ async function initMap() {
   }
 
   function closeHighlight() {
-    lastHighlightedMarker.content.classList.remove("highlight");
-    lastHighlightedMarker.zIndex = null;
-  }
-  let last_table = null;
-  function toggleTable(tableNumber) {
+    removeHighlight = lastHighlightedMarker;
     
-      var tableId = 'table' + tableNumber;
-      var table = document.getElementById(tableId);
-      
-      if(last_table != null){
-        last_table.style.display = 'none';
+
+  }
+  
+  var last_table = null;
+
+  
+  function toggleTable(tableNumber) {
+      const id = tableNumber.id;
+      console.log(tableNumber.id)
+      console.log(last_table)
+      console.log(document.getElementById("istasyon"+last_table))
+      if(id.includes("istasyon")){
+        document.getElementById("istasyon"+last_table).style.display = "block";
+        document.getElementById("lokasyon"+last_table).style.display = "none";
+        document.getElementById("hizmetler"+last_table).style.display = "none";
       }
-      table.style.display = 'block';
-      last_table = table;
+      else if(id.includes("lokasyon")){
+        document.getElementById("lokasyon"+last_table).style.display = "block";
+        document.getElementById("istasyon"+last_table).style.display = "none";
+        document.getElementById("hizmetler"+last_table).style.display = "none";
+      }
+      else if(id.includes("hizmetler")){
+        document.getElementById("hizmetler"+last_table).style.display = "block";
+        document.getElementById("istasyon"+last_table).style.display = "none";
+        document.getElementById("lokasyon"+last_table).style.display = "none";
+      }
+          
       
+    
       
     
   }
   
   function buildContent(property) {
     const content = document.createElement("div");
-  
     content.classList.add("property");
+    content.id = property.id;
     content.innerHTML = `
     <div class="icon">
     <i aria-hidden="true" class="fa-solid fa-${property.type}" title="${property.type}"></i>
 </div>
 <div class="details">
   <div class="btn-container">
-  <button onclick = toggleTable(${property.id})>İstasyon</button>
-  <button onclick = toggleTable(${property.id+1})>Lokasyon</button>
-  <button onclick = toggleTable(${property.id+2})>Hizmetler</button>
+  <button onclick = toggleTable(istasyon${property.id})>İstasyon</button>
+  <button onclick = toggleTable(lokasyon${property.id})>Lokasyon</button>
+  <button onclick = toggleTable(hizmetler${property.id})>Hizmetler</button>
     
-    <i onclick = closeHighlight() style="position:absolute;
-    right: 10px; top: 15px;" class="fa-solid fa-circle-xmark fa-2xl"></i>
+    <button onclick = closeHighlight() style="position:absolute;
+    right: 10px; top: 15px;" class="fa-solid fa-circle-xmark fa-2xl"></button>
 
 
   </div>
   <div class="descs">
-    <div id="table${property.id}" style="display:none">
+    <div id="istasyon${property.id}" style="display:block">
     <table  width='500px' class="table-hover" >
       <tr>
         <th>İstasyon Kodu</th>
         <td>${property.code}</td>
-      </tr>
-      <tr>
-        <th>Şehir</th>
-        <td>${property.city}</td>
       </tr>
       <tr>
         <th>Model Adı</th>
@@ -154,14 +179,15 @@ async function initMap() {
         <th>Soket</th>
         <td>${property.plug_type} / ${property.plug_number} soket mevcut </td>
       </tr>
-      <tr>
-        <th>Durum</th>
-        <td>${property.aktiflik}</td>
-      </tr>
+
     </table>
     </div>
-    <div id="table${property.id +1}" style="display:none">
+    <div id="lokasyon${property.id}" style="display:none">
     <table  width='500px' class="table-hover" >
+    <tr>
+    <th>Şehir</th>
+    <td>${property.city}</td>
+  </tr>
     <tr>
       <th>Adres</th>
       <td>${property.address}</td>
@@ -178,11 +204,15 @@ async function initMap() {
 
   </table>
   </div>
-    <div id="table${property.id+2}" style="display:none">
+    <div id="hizmetler${property.id}" style="display:none">
     <table  width='500px' class="table-hover" >
     <tr>
+    <th>Durum</th>
+    <td>${property.aktiflik}</td>
+  </tr>
+    <tr>
       <th>Hizmet Günleri</th>
-      <td>${property.hizmetsaati}</td>
+      <td style="width:350px">${property.hizmetsaati}</td>
     </tr>
     <tr>
     <th>Otopark Sayısı</th>
@@ -209,7 +239,8 @@ async function initMap() {
   
   const properties = [
     
-   
+  
   ];
   
   initMap();
+
